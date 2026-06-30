82d7fcef1d566a48c6d8b79a1b139699ebc2128c
Shahz-ft <shahanabeevi12@gmail.com>
2026-06-30 22:05:48 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -149,8 +149,8 @@ Rules:
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
